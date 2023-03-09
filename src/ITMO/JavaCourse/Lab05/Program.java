534a7c98c5707f38c44f15fde5ea78692da1b28e
FilippMaksimov <116656010+FilippMaksimov@users.noreply.github.com>
2023-03-09 23:26:28 +0300

    lab05

--- src/ITMO/JavaCourse/Lab05/Program.java
+++ src/ITMO/JavaCourse/Lab05/Program.java
@@ -10,11 +10,11 @@ public class Program {
         String longWord = null;
         int length = 0;
         String[] words = text.split(" ");
-        for (int i=0; i<=(words.length-1); i++) {
+        for (int i = 0; i <= (words.length - 1); i++) {
             words[i] = words[i].toLowerCase();
         }
         Arrays.sort(words);
-        for (String word: words) {
+        for (String word : words) {
             if (word.length() > length) {
                 length = word.length();
                 longWord = word;
@@ -22,6 +22,7 @@ public class Program {
         }
         return longWord;
     }
+
     public static Boolean palindromeCheck(String word) {
         System.out.println("\n" + "Второе задание");
         System.out.println("Является ли слово полиндромом");
@@ -29,30 +30,33 @@ public class Program {
         StringBuilder revWord = new StringBuilder(word);
         return (origWord.toString().equals(revWord.reverse().toString()));
     }
+
     public static String textEdit(String text) {
         System.out.println("\n" + "Третье задание");
         String censorText = text.replaceAll("бяка", "[Вырезано цензурой]");
         return censorText;
     }
+
     public static Integer inputQuantity(String mainString, String subString) {
         System.out.println("\n" + "Четвёртое задание");
         System.out.println("Количество вхождений " + subString.toUpperCase() + " в тексте:");
         int quantity = 0;
         String[] text = mainString.split(" ");
-        for (String item: text) {
+        for (String item : text) {
             if (item.contains(subString.toLowerCase()) || item.contains(subString.toUpperCase())) {
                 quantity++;
             }
         }
         return quantity;
     }
+
     public static String textInversion(String text) {
         System.out.println("\n" + "пятое задание");
         System.out.println("The given string is: " + text);
         System.out.println("The string reversed word by word is:");
         String[] words = text.split(" ");
         StringBuilder reversedText = new StringBuilder();
-        for (String word: words) {
+        for (String word : words) {
             StringBuilder reveredWord = new StringBuilder(word);
             reversedText.append(String.valueOf(reveredWord.reverse().append(" ")));
         }
